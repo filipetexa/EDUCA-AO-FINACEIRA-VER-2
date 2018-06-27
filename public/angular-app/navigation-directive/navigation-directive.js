e35d7f6133aae9a28601f6d17cbb92188123390f
filipe texa <filipeteixeirasouza@gmail.com>
2018-06-26 21:11:49 -0300

    Criaçao da base da nova navigation bar

--- public/angular-app/navigation-directive/navigation-directive.js
+++ public/angular-app/navigation-directive/navigation-directive.js
@@ -1,11 +1,21 @@
 angular
-.module('educaFi')
-.directive('mhNavigation', mhNavigation);
+    .module('educaFi')
+    .directive('mhNavigation', mhNavigation);
 
 
-function mhNavigation(){
-    return{
+function mhNavigation() {
+    return {
         restrict: 'E',
-        templateUrl:'angular-app/navigation-directive/navigation-directive.html'
+        templateUrl: 'angular-app/navigation-directive/navigation-directive.html'
+    }
+}
+
+
+function log() {
+    var log = document.getElementById("login").style.display
+    if (log == "none") {
+        document.getElementById("login").style.display = "block";
+    } else {
+        document.getElementById("login").style.display = "none";
     }
 }
